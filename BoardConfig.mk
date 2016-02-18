c7b0a087a0521d58dd7cb759d80ed794cd6b19e0
DavyA <davideabiuso@ymail.com>
2016-02-18 17:29:58 +0100

    fix twrp.fstab
    Change-Id: I8974221f0cc1c58ed8f5e606e8bbe76ac5a850c3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,9 +54,9 @@ TARGET_KERNEL_CONFIG := opx_defconfig
 TARGET_KERNEL_SOURCE := kernel/oneplus/msm8974
 
 # Enable DIAG on debug builds
-ifneq ($(TARGET_BUILD_VARIANT),user)
-TARGET_KERNEL_ADDITIONAL_CONFIG:= cyanogenmod_debug_config
-endif
+#ifneq ($(TARGET_BUILD_VARIANT),user)
+#TARGET_KERNEL_ADDITIONAL_CONFIG:= cyanogenmod_debug_config
+#endif
 
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
